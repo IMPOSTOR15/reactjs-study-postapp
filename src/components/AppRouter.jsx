4811f67c4d1c0b024f44640138c55e2c3697e82c
Andrey Dagaev <dandr212@gmail.com>
2023-04-24 19:39:10 +0300

    add post page with comments

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -3,12 +3,15 @@ import { Routes, Route } from "react-router-dom";
 import About from "../pages/About";
 import Posts from "../pages/Posts";
 import Error from "../pages/Error";
+import PostidPage from '../pages/PostidPage';
 const AppRouter = () => {
     return (
         <Routes>
             <Route path="/about" element={<About/>}/>
-            <Route path="/posts" element={<Posts/>}/>
+            <Route exact path="/posts" element={<Posts/>}/>
+            <Route exact path="/posts/:id" element={<PostidPage/>}/>
             <Route path="/error" element={<Error/>}/>
+            <Route path="/" element={<Posts/>}/>
             <Route path="*" element={<Error/>} />
         </Routes>
     );
---
+++ src/pages/PostidPage.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { useFetching } from '../hooks/useFetching';
+import PostService from '../API/PostService';
+import Loader from '../components/UI/Loader/Loader';
+
+const PostidPage = () => {
+    const params = useParams();
+    const [post, setPost] = useState({});
+    const [comments, setComments] = useState([])
+
+    const [fetchPostById, isLoading, error] = useFetching(async (id) => {
+        const responce = await  PostService.getById(id);
+        setPost(responce.data);
+    })
+    const [fetchComments, isComLoading, comError] = useFetching(async (id) => {
+        const responce = await  PostService.getCommentsByPostId(id);
+        setComments(responce.data);
+    })
+
+    useEffect(() => {
+        fetchPostById(params.id)
+        fetchComments(params.id)
+    },[])
+    return (
+        <div>
+            <h1>Вы открыли страницу поста c ID = {params.id}</h1>
+            {isLoading
+                ? <Loader/>
+                : <div><h2>{post.id}. {post.title}</h2><p>{post.body}</p></div>
+
+            }
+            <h1>Коментарии</h1>
+            {isComLoading
+                ? <Loader/>
+                : <div>
+                    {comments.map(comm =>
+                        <div style={{marginTop: '15px'}}>
+                            <h5>{comm.email}</h5>
+                            <div>{comm.body}</div>
+                        </div>
+                    )}
+                </div>
+
+            }
+        </div>
+    );
+};
+
+export default PostidPage;
