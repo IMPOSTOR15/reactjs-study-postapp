ffc012266c3404c0ee24925f174b2b680c91b9d9
Andrey Dagaev <dandr212@gmail.com>
2023-04-24 19:06:55 +0300

    add routing

---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <h1>
+            Это приложение создано для обучения базовым концепциям React
+        </h1>
+    );
+};
+
+export default About;
